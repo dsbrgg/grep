4ddc85d05afca62e553e1cb213b86aa10a87ee00
Diego Braga <dsbrgg@gmail.com>
2019-02-07 21:39:49 +0000

    chore: separate query and filename

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,5 +2,11 @@ use std::env;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  println!("{:#?}", args);
+
+  // cargo run <query> <filename>
+  let query = &args[1];
+  let filename = &args[2];
+
+  println!("Query -> {:#?}", query);
+  println!("Filename -> {:#?}", filename);
 }
