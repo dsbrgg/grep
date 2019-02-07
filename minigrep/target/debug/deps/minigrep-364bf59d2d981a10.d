7d810c2b3662c07dfaf5d19e1196dcae15aa6269
Diego Braga <dsbrgg@gmail.com>
2019-02-07 21:37:06 +0000

    fix: for real, fuck

--- minigrep/target/debug/deps/minigrep-364bf59d2d981a10.d
+++
@@ -1,5 +0,0 @@
-/Users/opuzzz/Documents/opuzzz/grep/minigrep/target/debug/deps/minigrep-364bf59d2d981a10: src/main.rs
-
-/Users/opuzzz/Documents/opuzzz/grep/minigrep/target/debug/deps/minigrep-364bf59d2d981a10.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/opuzzz/Documents/opuzzz/grep/minigrep/target/debug/minigrep.dSYM: /Users/opuzzz/Documents/opuzzz/grep/minigrep/src/main.rs
