794ce5b393ed2a9be31ae5ab7cc235f83a92ebd2
Diego Braga <dsbrgg@gmail.com>
2019-02-08 19:44:51 +0000

    chore: added default txt file to read from

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -7,6 +8,10 @@ fn main() {
   let query = &args[1];
   let filename = &args[2];
 
-  println!("Query -> {:#?}", query);
-  println!("Filename -> {:#?}", filename);
+  println!("In file: {:#?}", filename);
+
+  let contents = fs::read_to_string(filename)
+    .expect("Something went wrong when reading the file");
+
+  println!("With text: \n{}", contents);
 }
