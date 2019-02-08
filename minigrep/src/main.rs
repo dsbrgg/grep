2ece532756e8ec48a6be7b3f93712fde0e7112aa
Diego Braga <dsbrgg@gmail.com>
2019-02-08 19:59:15 +0000

    refactor: make config struct to represent query and filename

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,19 +5,25 @@ fn main() {
   let args: Vec<String> = env::args().collect();
 
   // cargo run <query> <filename>
-  let (query, filename) = parse_config(&args);
+  let config = parse_config(&args);
 
-  println!("In file: {:#?}", filename);
+  println!("Searching for: {:#?}", config.query);  
+  println!("In file: {:#?}", config.filename);
 
-  let contents = fs::read_to_string(filename)
+  let contents = fs::read_to_string(config.filename)
     .expect("Something went wrong when reading the file");
 
   println!("With text: \n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-  let query = &args[1];
-  let filename = &args[2];
+struct Config {
+  query: String,
+  filename: String,
+}
 
-  (query, filename)
+fn parse_config(args: &[String]) -> Config {
+  let query = args[1].clone();
+  let filename = args[2].clone();
+
+  Config { query, filename }
 }
