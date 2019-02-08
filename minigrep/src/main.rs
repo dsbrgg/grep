e163c86d6d28ea42a7d263309cf1cd1b2a01c586
Diego Braga <dsbrgg@gmail.com>
2019-02-08 20:05:03 +0000

    refactor: add new method to config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
   let args: Vec<String> = env::args().collect();
 
   // cargo run <query> <filename>
-  let config = parse_config(&args);
+  let config = Config::new(&args);
 
   println!("Searching for: {:#?}", config.query);  
   println!("In file: {:#?}", config.filename);
@@ -21,9 +21,11 @@ struct Config {
   filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-  let query = args[1].clone();
-  let filename = args[2].clone();
+impl Config {
+  fn new(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-  Config { query, filename }
+    Config { query, filename }
+  }
 }
