5cbfd3ad9718f4144e3049dff81cead5d949c55e
Diego Braga <dsbrgg@gmail.com>
2019-02-09 15:38:18 +0000

    tests: added unit testing

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,3 +29,37 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   // we’re calling run for its side effects only; it doesn’t return a value we need.
   Ok(())
 }
+
+// This is important! The data referenced by a slice needs to be valid for the reference to be valid; 
+// if the compiler assumes we’re making string slices of query rather than contents,
+// it will do its safety checking incorrectly
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents)
+    )
+  }
+}
