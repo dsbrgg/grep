0e35f848c5129c29833fc268839eb209d3e5b002
Diego Braga <dsbrgg@gmail.com>
2019-02-09 13:06:56 +0000

    refactor: pass reading file logic to its own function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,24 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
-
-fn main() {
-  let args: Vec<String> = env::args().collect();
-
-  // cargo run <query> <filename>
-  let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {}", err);
-    process::exit(1);
-  });
-
-  println!("Searching for: {:#?}", config.query);  
-  println!("In file: {:#?}", config.filename);
-
-  let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong when reading the file");
-
-  println!("With text: \n{}", contents);
-}
+use std::error::Error;
 
 struct Config {
   query: String,
@@ -34,4 +17,32 @@ impl Config {
 
     Ok(Config { query, filename })
   }
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+
+  // cargo run <query> <filename>
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
+
+  println!("Searching for: {:#?}", config.query);  
+  println!("In file: {:#?}", config.filename);
+
+  run(config);
+}
+
+//  For now, just know that Box<dyn Error> means the function will return a type that implements the Error trait,
+//  but we don’t have to specify what particular type the return value will be
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  // ? will return the error value from the current function for the caller to handle
+  let contents = fs::read_to_string(config.filename)?;
+
+  println!("With text: \n{}", contents);
+
+  // using () like this is the idiomatic way to indicate that
+  // we’re calling run for its side effects only; it doesn’t return a value we need.
+  Ok(())
 }
